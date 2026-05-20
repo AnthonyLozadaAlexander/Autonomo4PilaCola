8cd641e658e222ba4346764d34a987f4b7593803
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-20 06:10:15 -0500

    Cola
    Se agrego metodo recursivo para devolver el utimo elemento de la Cola

--- src/logicaRecursividad/RecursividadCola.java
+++ src/logicaRecursividad/RecursividadCola.java
@@ -1,5 +1,39 @@
 package logicaRecursividad;
 
-public class RecursividadCola {
+import tadCola.*;
 
+public class RecursividadCola {
+	
+	public static <T> T ultimoElemento(Cola<T> cola) throws ColaVacia {
+		
+		Cola<T> aux;
+		aux = cola;
+		
+		if(aux.colaVacia()) {
+			return null;
+		}
+		
+		/*
+		 *   Sacamos los primeros elementos de la cola
+		 */
+		
+		T guardar = aux.desencolar(); 
+		
+		T resultRecursivo = ultimoElemento(cola); // mandamos a desencolar el resto de la cola 
+		
+		cola.encolar(guardar);
+		
+		if(resultRecursivo == null) {
+			
+			return guardar;  // si el resultado de la recursividad es null, quiere decir que el ultimo elemento desencolado fue el ultimo en guardar
+		}else {
+			return resultRecursivo;
+		}
+		
+		
+		
+		
+		
+	}
+	
 }
